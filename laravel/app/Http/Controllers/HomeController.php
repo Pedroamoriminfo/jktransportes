4ebb18670242afb3d348b0b32bb55e675dfa5154
= <=>
2024-10-26 13:59:37 -0300

    docs: first commit

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -2,8 +2,13 @@
 
 
 namespace App\Http\Controllers;
+
 use App\Services\MailService;
 use Illuminate\Http\Request;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class HomeController extends Controller
 {
@@ -44,15 +49,43 @@ class HomeController extends Controller
      */
     public function reservaCreate(Request $request)
     {
-        $de = $request->input('email');
-        $nomeDe = $request->input('name');
-        $para = env('EMAIL_NAME');
-        $assunto = 'Assunto do E-mail';
-        $mensagemHtml = '<h1>Olá!</h1><p>Esta é uma mensagem em HTML.</p>';
-        $mensagemTexto = 'Olá! Esta é a mensagem em texto, caso o HTML não funcione.';
+        $mail = new PHPMailer(true);
 
-        $resultado = $this->mailService->enviarEmail($de, $nomeDe,$para, $assunto, $mensagemHtml, $mensagemTexto);
-        return response()->json(['message' => $resultado]);
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       ='smtp.google.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   =  'amoriminformatica01@gmail.com';                    //SMTP username
+            $mail->Password   = '@Asj20508';                           //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            // $mail->setFrom('gracapersonalizadosrdo@gmail.com', 'Mailer');
+            $mail->addAddress('drivedajo2020@gmail.com', 'Joe User');     //Add a recipient
+            $mail->addAddress('gracapersonalizadosrdo@gmail.com', 'Joe User');     //Add a recipient
+            // $mail->addAddress('ellen@example.com');               //Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
     /**
      * Show the form for creating a new resource.
@@ -66,8 +99,7 @@ class HomeController extends Controller
         $mensagemHtml = '<h1>Olá!</h1><p>Esta é uma mensagem em HTML.</p>';
         $mensagemTexto = 'Olá! Esta é a mensagem em texto, caso o HTML não funcione.';
 
-        $resultado = $this->mailService->enviarEmail($de, $nomeDe,$para, $assunto, $mensagemHtml, $mensagemTexto);
+        $resultado = $this->mailService->enviarEmail($de, $nomeDe, $para, $assunto, $mensagemHtml, $mensagemTexto);
         return response()->json(['message' => $resultado]);
     }
-
 }
--- laravel/resources/views/site/contato.blade.php
+++ laravel/resources/views/site/contato.blade.php
@@ -2,36 +2,40 @@
 <br>
 @section('home')
 <br>
-<section class="contato py-5" data-aos="fade-up">
-    <div class="container">
-        <h1 class="text-center mb-4">Fale Conosco</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <form>
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Nome</label>
-                        <input type="text" class="form-control" id="name" placeholder="Seu nome" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">E-mail</label>
-                        <input type="email" class="form-control" id="email" placeholder="Seu e-mail" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="phone" class="form-label">Telefone</label>
-                        <input type="tel" class="form-control" id="phone" placeholder="Seu telefone">
-                    </div>
-                    <div class="mb-3">
-                        <label for="message" class="form-label">Mensagem</label>
-                        <textarea class="form-control" id="message" rows="5" placeholder="Sua mensagem" required></textarea>
-                    </div>
-                    <div class="d-grid">
-                        <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
-                    </div>
-                </form>
+    <section class="contato py-5" data-aos="fade-up">
+        <div class="container">
+            <h1 class="text-center mb-5">Fale Conosco</h1>
+            <div class="row justify-content-center">
+                <div class="col-12 mb-4">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4383.931810556852!2d-41.92258415412777!3d-22.487948924507403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x97b5c3f04cb47f%3A0x18bff567445c74c!2sJK%20Transportes%20%7C%20Rio%20das%20Ostras%20-%20RJ!5e0!3m2!1spt-BR!2sbr!4v1729956844111!5m2!1spt-BR!2sbr"
+                        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-md-8">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nome</label>
+                            <input type="text" class="form-control" id="name" placeholder="Seu nome" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input type="email" class="form-control" id="email" placeholder="Seu e-mail" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="phone" class="form-label">Telefone</label>
+                            <input type="tel" class="form-control" id="phone" placeholder="Seu telefone">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message" class="form-label">Mensagem</label>
+                            <textarea class="form-control" id="message" rows="5" placeholder="Sua mensagem" required></textarea>
+                        </div>
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
-
-
